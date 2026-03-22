8a79cd053f9185455ce9f0e21c744e16d92b7284
Utkarsh9721 <raiutkarsh544@gmail.com>
2026-03-22 21:33:23 +0530

    Update API URL to use environment variable

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -78,7 +78,8 @@ const Contact = () => {
   setSubmitStatus(null);
 
   try {
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const response = await fetch(`${API_URL}/contact`,{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
