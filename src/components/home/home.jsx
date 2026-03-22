985d171ee16dc35200232bdc8951806972e4c75e
Utkarsh9721 <raiutkarsh544@gmail.com>
2026-03-23 01:18:43 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -15,9 +15,7 @@ const Home = () => {
         Embark on unforgettable adventures with TransXs. From hidden gems to iconic landmarks, we guide you to experiences that ignite your wanderlust. Explore curated travel guides, book seamless trips, and uncover local cultures with insider tips. Whether you seek thrilling adventures, serene escapes, or cultural immersions, your next journey starts here.
       </p>
       <div className="explore" data-aos="zoom-in" data-aos-delay="600">
-        <button>
-          Explore More <img src={Arrow} alt="arrow" height="30px" />
-        </button>
+        
       </div>
     </div>
   );
